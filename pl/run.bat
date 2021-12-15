8a6fdac7563f3beb3b7cf58f0a7baec1df4d7fb0
DESKTOP-T526RFM\lenovo <akshatjaiswal521@gmail.com>
2021-12-15 13:11:16 +0530

    minor changes in run.bat

--- pl/run.bat
+++ pl/run.bat
@@ -1,1 +1,1 @@
-java -classpath ..\bl\build\libs\bl.jar;..\common\build\libs\common.jar;..\dbdl\build\libs\dbdl-1.0.jar;build\libs\pl.jar;jars\flatlaf\flatlaf-1.6.jar;jars\itext\*;..\dbdl\jars\mysql\mysql.jar;. com.thinking.machines.hr.pl.Main
+java -classpath ..\bl\build\libs\bl.jar;..\common\build\libs\common.jar;..\dbdl\build\libs\dbdl-1.0.jar;build\libs\pl.jar;jars\flatlaf\flatlaf-1.6.jar;jars\itext\*;..\dbdl\jars\mysql\mysql.jar;..\dl\dist\hr-dl-1.0.jar;. com.thinking.machines.hr.pl.Main
